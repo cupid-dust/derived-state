237d790cdf1cdb60626280222ad3fa9178f04312
cupid_dust <ahsanmansoor7572@gmail.com>
2022-07-13 14:36:54 +0500

    derived state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo } from 'react';
+
+type UserData = {
+  id: number;
+  name: string;
+};
 
 function App() {
+  const [users, setUsers] = useState<UserData[]>([
+    { id: 1, name: 'Kyle' },
+    { id: 2, name: 'John' },
+  ]);
+  const [selectedUserId, setSelectedUserId] = useState<number>();
+
+  const selectedUser: UserData | any = useMemo(() => {
+    return users.find((user: UserData) => user.id === selectedUserId);
+  }, [users, selectedUserId]);
+
+  const selectUser = (id: number) => {
+    setSelectedUserId(id);
+  };
+
+  const updateUser = (id: number, name: string) => {
+    setUsers((prevUsers: UserData[]) => {
+      const newUsers = [...prevUsers];
+      const user: UserData | any = newUsers.find((user) => user.id === id);
+      user.name = name;
+      return newUsers;
+    });
+  };
+
+  useEffect(() => {
+    selectUser(1);
+    updateUser(1, 'Kate');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user) => user.name).join(', ')}
+      <p>{JSON.stringify(selectedUser)}</p>
     </div>
   );
 }
